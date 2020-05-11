9091905b6aa24451f82122bb55f5495f46d60ed5
sunnythree <1318288270@qq.com>
2020-05-11 09:39:01 +0800

    fix bug

--- eval.py
+++ eval.py
@@ -66,9 +66,9 @@ def eval(args):
         error_num += e
         miss_num += m
         print("c,e,m=",correct_num, error_num, miss_num)
-    print("correct rate: "+str(correct_num/all_num))
-    print("error rate: " + str(error_num / all_num))
-    print("miss rate: " + str(all_num / all_num))
+    print("correct rate: "+str(correct_num / all_num*100)+"%")
+    print("error rate: " + str(error_num / all_num*100)+"%")
+    print("miss rate: " + str(all_num / all_num*100)+"%")
 
 if __name__=='__main__':
     eval(parse_args())
